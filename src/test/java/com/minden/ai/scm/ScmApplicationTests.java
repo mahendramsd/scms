cec82063d098919758dd47c50bbf098138b4982f
Mahendra <mahendrasridayarathna@gmail.com>
2024-04-28 09:59:09 +0530

    initial project setup

---
+++ src/test/java/com/minden/ai/scm/ScmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minden.ai.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
